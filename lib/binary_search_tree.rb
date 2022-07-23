092a78230e653ce2d8397d88071c70071adaebfe
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-23 16:05:17 -0700

    commit tree 77c286b10370e7bc0ccc5e7a7cbb8d33f7f3bcba

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,60 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+
+    if !@root
+      return nil
+    end
+
+    current_node = @root
+
+    while true
+      if current_node.value == value
+        return current_node
+      elsif current_node.value > value       
+        if current_node.left
+          current_node = current_node.left
+        else
+          return nil
+        end
+      elsif current_node.value < value
+        if current_node.right
+          current_node = current_node.right
+        else 
+          return nil
+        end
+      end 
+    end
+
   end
 
   def insert(value)
-    # your code here
+    
+    node = Node.new(value)
+
+    if !@root
+      @root = node
+      return node
+    end 
+
+    current_node = @root
+
+    while true
+      if current_node.value = value
+        return nil
+      elsif current_node.value < value && current_node.right = nil
+        current_node.right = node
+        return node
+      elsif current_node.value < value && current_node.right
+        current_node = current_node.right
+      elsif current_node.value > value && current_node.left = nil
+        current_node.left = node
+        return node
+      elsif current_node.value > value && current_node.left
+        current_node = current_node.left
+      end
+    end
+
   end
 
 end 
